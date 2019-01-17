4f05ee078844ef308a934095f4ca02dad2443566
choonewza <choonewza@gmail.com>
2019-01-17 11:25:55 +0700

    01_17_2019_11_25

---
+++ config/keys-dev.js
@@ -0,0 +1,7 @@
+// dev.js - don't commit this
+module.exports = {
+  googleClientID: "",
+  googleClientSecret: "",
+  mongoURI: "mongodb://test:password8@xxx.mlab.com:51614/loraiot",
+  cookieKey: "asdfaskl;g12asd6f65asdfa225asdf"
+};
